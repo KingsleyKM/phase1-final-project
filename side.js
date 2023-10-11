c70cabf9aa97876b2433d16f336d9c376066fe09
Kingsley Maina <mainakingsley7@gmail.com>
2023-10-11 18:16:59 +0300

    Second commit

--- side.js
+++ side.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
+    //fetch data from the public api
     fetch("https://cataas.com/cat?json=true")
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            //display the image on the page
             let imageDiv = document.createElement("div")
             imageDiv.className = "images"
             imageDiv.innerHTML = `
          <img src="https://cataas.com/${data.url}">
         `
             document.querySelector(".image1").appendChild(imageDiv)
+            //eventlistener to change the image
             document.querySelector("#random").addEventListener("click", () => {
                 changeButton();
             })
         })
+        //function that changes the image displayed and replaces it
     function changeButton() {
         fetch("https://cataas.com/cat?json=true")
             .then(res => res.json())
@@ -26,9 +30,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.querySelector(".image1").appendChild(imageDiv2)
             })
     }
+    //eventlistener to the gifs button
     document.querySelector("#gifs").addEventListener("click", () => {
         giveGif();
     })
+    //function that changes the main dispay into a gif
     function giveGif() {
         fetch("https://cataas.com/cat?type=gif&json=true")
             .then(res => res.json())
@@ -37,31 +43,29 @@ document.addEventListener("DOMContentLoaded", () => {
                 let gifsDiv = document.createElement("div")
                 gifsDiv.className = `replacer`
                 gifsDiv.innerHTML = `
-               <img src="https://cataas.com/gif">
+               <img src="https://cataas.com/cat/gif">
                `
                 document.querySelector(".image1").appendChild(gifsDiv)
 
             });
     }
-    fetch("https://cataas.com/api/cats?tags=cute")
+    //fetch the tags from the public API
+   // function imageTag(){
+    fetch("https://cataas.com/api/tags")
     .then(res => res.json())
-    .then(value => {
-        console.log(value)
-        let tags = document.createElement("li")
-        let list = value.tags
-        console.log(list)
-        tags.className=`list_${value.id}`
-        
-        function loopingTags(){
-            let text=''
-            for(i = 0;i < list.length;i++){
-            text += list[1]
-            }
-        }
-
-
-        tags.innerHTML=`
-         <p>${value.tags}
-        `
+    .then(element=> {
+        console.log(element)
+        const val = document.querySelector("input").value;
+        console.log(val)
+        console.log(element.tag)
+        document.getElementById("tag").addEventListener("click",()=>{
+           document.querySelector(".image1").innerHTML=``
+           let tagDiv = document.createElement("div")
+           tagDiv.className="tagsList"
+           tagDiv.innerHTML=`
+           <img src= "https://cataas.com/cat/cute">
+           `
+           document.querySelector(".image1").appendChild(tagDiv)
+        })
     })
 })
